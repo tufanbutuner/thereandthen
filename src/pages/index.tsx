ae87de32499053cf085e9a1bb0c68401dfd8845f
Tufan Butuner <43154254+tufanbutuner@users.noreply.github.com>
2023-07-27 00:18:09 +0100

    update ico

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ export default function Home() {
           content="Development Studio brought to you by Tufan"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.svg" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main} ${dmsans.className}`}>
         <div>
