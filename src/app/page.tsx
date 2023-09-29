10a3c02b1ac139c07f8541ca1d4cf7f6f3d39513
Tufan.Butuner <Tufan.Butuner@transformuk.com>
2023-09-29 17:42:35 +0100

    update homepage mobile styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,80 +25,66 @@ export default function Page() {
   };
 
   return (
-    <div>
-      <div className="container main">
-        <div className="section-1">
-          <Image src={Logo} alt="Logo" width={200} />
-          <p>Web and marketing solutions for small businesses.</p>
-          <Card
-            text="Services"
-            backgroundColor={theme.colors.pastel.paleblue}
-            textColour={theme.colors.pastel.hotred}
-            imagePath={money}
-            onClick={() => handleCardClick("services")}
-          />
-        </div>
-        <div className="section-2">
-          <Card
-            text="Who we are"
-            backgroundColor={theme.colors.pastel.babyblue}
-            textColour={theme.colors.pastel.red}
-            imagePath={hello}
-            onClick={() => handleCardClick("who-we-are")}
-          />
-          <Card
-            text="Testimonials"
-            backgroundColor={theme.colors.pastel.lightorange}
-            textColour={theme.colors.pastel.beige}
-            imagePath={seated}
-            onClick={() => handleCardClick("testimonials")}
-          />
-        </div>
-        <div className="section-3">
-          <Card
-            text="Clients"
-            backgroundColor={theme.colors.pastel.palegreen}
-            textColour={theme.colors.pastel.peach}
-            imagePath={clients}
-            onClick={() => handleCardClick("Clients")}
-          />
-          <Card
-            text="FAQ"
-            backgroundColor={theme.colors.pastel.orange}
-            textColour={theme.colors.pastel.beige}
-            imagePath={confused}
-            onClick={() => handleCardClick("FAQ")}
-          />
-        </div>
-        <div className="section-4">
-          <Card
-            text="Impact"
-            backgroundColor={theme.colors.pastel.peach}
-            textColour={theme.colors.pastel.babyblue}
-            imagePath={walking}
-            onClick={() => handleCardClick("Impact")}
-          />
-          <Card
-            text="Contact Us"
-            backgroundColor={theme.colors.pastel.hotpink}
-            textColour={theme.colors.pastel.peach}
-            imagePath={contact}
-            onClick={() => handleCardClick("Contact Us")}
-          />
-        </div>
+    <div className="container main">
+      <div className="section-1">
+        <Image src={Logo} alt="Logo" width={200} />
+        <p>Web and marketing solutions for small businesses.</p>
+        <Card
+          text="Services"
+          backgroundColor={theme.colors.pastel.paleblue}
+          textColour={theme.colors.pastel.hotred}
+          imagePath={money}
+          onClick={() => handleCardClick("services")}
+        />
+      </div>
+      <div className="section-2">
+        <Card
+          text="Who we are"
+          backgroundColor={theme.colors.pastel.babyblue}
+          textColour={theme.colors.pastel.red}
+          imagePath={hello}
+          onClick={() => handleCardClick("who-we-are")}
+        />
+        <Card
+          text="Testimonials"
+          backgroundColor={theme.colors.pastel.lightorange}
+          textColour={theme.colors.pastel.beige}
+          imagePath={seated}
+          onClick={() => handleCardClick("testimonials")}
+        />
+      </div>
+      <div className="section-3">
+        <Card
+          text="Clients"
+          backgroundColor={theme.colors.pastel.palegreen}
+          textColour={theme.colors.pastel.peach}
+          imagePath={clients}
+          onClick={() => handleCardClick("Clients")}
+        />
+        <Card
+          text="FAQ"
+          backgroundColor={theme.colors.pastel.orange}
+          textColour={theme.colors.pastel.beige}
+          imagePath={confused}
+          onClick={() => handleCardClick("FAQ")}
+        />
+      </div>
+      <div className="section-4">
+        <Card
+          text="Impact"
+          backgroundColor={theme.colors.pastel.peach}
+          textColour={theme.colors.pastel.babyblue}
+          imagePath={walking}
+          onClick={() => handleCardClick("Impact")}
+        />
+        <Card
+          text="Contact Us"
+          backgroundColor={theme.colors.pastel.hotpink}
+          textColour={theme.colors.pastel.peach}
+          imagePath={contact}
+          onClick={() => handleCardClick("Contact Us")}
+        />
       </div>
     </div>
   );
-}
-
-{
-  /* <Head>
-        <title>There and Then</title>
-        <meta
-          name="description"
-          content="Web solutions and marketing for small businesses"
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head> */
 }
