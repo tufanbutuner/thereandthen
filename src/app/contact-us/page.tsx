e80b8558df9c5c1a66541b8257c33fea42ac946e
Tufan.Butuner <Tufan.Butuner@transformuk.com>
2023-10-05 02:10:28 +0100

    Contact and services page changes

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -1,4 +1,17 @@
 import RedirectToHome from "@/components/Redirect";
+import { BiLogoInstagram } from "react-icons/bi";
+import { FaXTwitter } from "react-icons/fa6";
+
+const socials = [
+  {
+    username: "therenthenco",
+    icon: <BiLogoInstagram size={40} />,
+  },
+  {
+    username: "therenthenco",
+    icon: <FaXTwitter size={36} />,
+  },
+];
 
 export default function Contact() {
   return (
@@ -6,6 +19,17 @@ export default function Contact() {
       <div className="page-header">
         <p>Contact Us</p>
         <RedirectToHome />
+      </div>
+      <div className="contact-container">
+        <div className="contact-text">
+          <h3>Here's where you can find us</h3>
+          {socials.map((social, index) => (
+            <div className="contact-icons-container" key={index}>
+              {social.icon}
+              <p>{social.username}</p>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -23,7 +23,6 @@ export default function Services() {
       </div>
       <div className="who-we-are-container fade-in">
         <div className="services-text fade-in">
-          <p>We offer a wide range of web and marketing solutions.</p>
           <h3>Web development that wows</h3>
           <p>
             We’ve got you covered on all your website needs, from a website
@@ -66,24 +65,12 @@ export default function Services() {
         <div className="services-text fade-in">
           <h3>Crush the search results</h3>
           <p>
-            We’ve got you covered on all your website needs, from a website
-            revamp to maintenance, to building a website from the ground up,
-            we’re excited to show you our creativity and what we can do for your
-            business.
+            Want to climb the Google ranks? Is your Google Ads account stuck in
+            the mud? We’ll help you climb the search ladder and make your
+            website shine bright in the search results. By optimising your web
+            content and tweaking your keywords, we’ll make sure to keep up with
+            the ever changing world of search and SEO so you don’t need to.
           </p>
-          <div className="services-list fade-in">
-            <p>
-              We aren’t just about creating generic websites but we create
-              experiences unique to each client and their needs:
-            </p>
-            <ul>
-              <li>E-commerce</li>
-              <li>Booking system</li>
-              <li>Blog</li>
-              <li>Portfolio</li>
-              <li>And much more...</li>
-            </ul>
-          </div>
         </div>
       </div>
     </div>
