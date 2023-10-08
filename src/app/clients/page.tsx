e638c8bf0e1458953ad549f35b9c58ac6328dbf3
Tufan.Butuner <Tufan.Butuner@transformuk.com>
2023-10-08 03:45:04 +0100

    initial Clients page

--- src/app/clients/page.tsx
+++ src/app/clients/page.tsx
@@ -1,4 +1,20 @@
 import RedirectToHome from "@/components/Redirect";
+import Link from "next/link";
+
+const clientList = [
+  {
+    name: "Candy Crown",
+    url: "https://thecandycrown.com/",
+  },
+  {
+    name: "La Planchette",
+    url: "https://www.laplanchette.uk",
+  },
+  {
+    name: "Viveza Futbol",
+    url: "https://www.vivezafutbol.com",
+  },
+];
 
 export default function Clients() {
   return (
@@ -6,6 +22,14 @@ export default function Clients() {
       <div className="page-header">
         <p>Clients</p>
         <RedirectToHome />
+      </div>
+      <div className="clients-container">
+        <h1>Meet our clients</h1>
+        {clientList.map((client, index) => (
+          <Link href={client.url} target="_blank" key={index}>
+            <p>{client.name}</p>
+          </Link>
+        ))}
       </div>
     </div>
   );
