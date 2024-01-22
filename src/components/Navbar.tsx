d77907c1d03eca7b822ad4f2c8e0fdb8bd027db6
Tufan.Butuner <Tufan.Butuner@transformuk.com>
2024-01-22 20:17:38 +0000

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,13 @@
 import images from "@/utils/images";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Navbar() {
   return (
     <div className="navbar-container">
-      <Image src={images.logo} alt="Logo" width={200} />
+      <Link href={"/"}>
+        <Image src={images.logo} alt="Logo" width={200} />
+      </Link>
       <p>Web and marketing solutions for small businesses.</p>
     </div>
   );
